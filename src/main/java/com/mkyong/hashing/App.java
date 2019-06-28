a18a2c4d20c6f64759b6c5ecdc5c7a57c316597a
bhabani <bhabani.dixit@gmail.com>
2019-06-28 05:32:09 +0000

    commit msg

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -8,9 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-	System.out.println( "Hello World!" );
-	
+        final String msg1="msg1";
+	System.out.println(lo);
+	System.out.println(Res_Bundle(msg1));
     }
 
 public String checkevenodd(int n){
